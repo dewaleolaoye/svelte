7a0455e7ed2362b2e2e3e490b806a51ff8b2aed5
dewaleolaoye <adewaleolaoye21@gmail.com>
2022-04-17 22:27:24 +0100

    feat: conditionals and reactive

--- src/Nested.svelte
+++ src/Nested.svelte
@@ -1,1 +1,17 @@
+<script>
+  let count = 0;
+
+  function handleIncrement () {
+    count += 1;
+  }
+
+  function handleClearCount () {
+    count = 0;
+  }
+</script>
+
 <p>How do you reuse a component here?</p>
+
+<button on:click={handleIncrement}>Click {count} {count <= 1 ? 'time' : 'times'}</button>
+
+<button on:click={handleClearCount}>Clear</button>
